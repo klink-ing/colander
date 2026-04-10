41ace205324ae68f15c9f2741c8a5cd1a437b10d
Klink <85062+dogmar@users.noreply.github.com>
2026-04-09 22:07:18 -0700

    Better theme-auto icon (#8)

--- website/src/components/ThemeToggle.tsx
+++ website/src/components/ThemeToggle.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import ThemeAuto from "#/components/icons/theme-auto.tsx";
 import { Button } from "#/components/ui/button";
 
 type ThemeMode = "light" | "dark" | "auto";
@@ -110,23 +111,7 @@ export default function ThemeToggle() {
           <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
         </svg>
       ) : (
-        <svg
-          className="size-5"
-          width="20"
-          height="20"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          aria-hidden="true"
-        >
-          <path d="M12 0v1.5M12 22.5v1.5M4.22 4.22l1.06 1.06M18.72 18.72l1.06 1.06M0 12h1.5M22.5 12h1.5M5.28 18.72l-1.06 1.06M18.72 4.22l1.06 1.06" />
-          <line x1="7.625" y1="17" x2="12" y2="7" strokeWidth="1" />
-          <line x1="16.375" y1="17" x2="12" y2="7" strokeWidth="1" />
-          <line x1="9.375" y1="13" x2="14.625" y2="13" strokeWidth="1" />
-        </svg>
+        <ThemeAuto className="size-4.5" />
       )}
     </Button>
   );
---
+++ website/src/components/icons/theme-auto.tsx
@@ -0,0 +1,30 @@
+import type { SVGProps } from "react";
+
+export default (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    clip-rule="evenodd"
+    fill-rule="evenodd"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-miterlimit="1.5"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g stroke="currentColor" stroke-width="2.5">
+      <path d="m1.052028 16.534799 2.98581-1.236764" />
+      <path d="m19.962162 8.701965 2.98581-1.236764" />
+      <path d="m7.465201 22.947972 1.236764-2.98581" />
+      <path d="m15.298035 4.037838 1.236764-2.98581" />
+      <path d="m16.534799 22.947972-1.236764-2.98581" />
+      <path d="m8.701965 4.037838-1.236764-2.98581" />
+      <path d="m22.947972 16.534799-2.98581-1.236764" />
+      <path d="m4.037838 8.701965-2.98581-1.236764" />
+    </g>
+    <path
+      d="m14.115356 14.881349h-4.109619l-.771972 2.293213h-2.437012l3.980957-11.155762h2.633789l3.950684 11.155762h-2.527832zm-.650878-1.922363-1.392579-4.389649-1.437988 4.389649z"
+      fill="currentColor"
+      fill-rule="nonzero"
+    />
+  </svg>
+);
